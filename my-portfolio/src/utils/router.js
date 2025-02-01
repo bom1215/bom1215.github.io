054b9dbe85bea4cb0ebdd6bac62dce38faae1438
bom1215 <bom1215@naver.com>
2025-02-01 21:06:43 +0900

    feat : add post-id on routers

--- my-portfolio/src/utils/router.js
+++ my-portfolio/src/utils/router.js
@@ -1,31 +1,12 @@
-// import { createMemoryHistory, createRouter } from 'vue-router'
-
-// import Home from '@/pages/About.vue'
-// import Portfolio from '@/pages/Portfolio.vue'
-// import About from '@/pages/About.vue'
-// import Post from '@/pages/Post.vue'
-
-// const routes = [
-//   { path: '/', component: Home },
-//   { path: '/about', component: About },
-//   { path: '/portfolio', component: Portfolio },
-//   { path: '/post', component: Post },
-// ]
-
-// const router = createRouter({
-//   history: createMemoryHistory(),
-//   routes,
-// })
-// export default router;
-
-// router/index.js 또는 router.js
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
   { path: '/', component: () => import('@/pages/Home.vue') },
   { path: '/about', component: () => import('@/pages/About.vue') },
   { path: '/portfolio', component: () => import('@/pages/Portfolio.vue') },
-  { path: '/post', component: () => import('@/pages/Post.vue') },
+  { path: '/post', component: () => import('@/pages/PostMain.vue') },
+  { path: '/post/:id', component: () => import('@/pages/Post.vue') },
+  
 ];
 
 const router = createRouter({
