a40f52864e3ca06b66c472620cfa02766aa27c1d
bom1215 <bom1215@naver.com>
2025-03-02 21:41:08 +0900

    vite press update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Junbeom blog",
-  description: "blog",
+  lang: 'eng',
+  title: "Junbeom Lee",
+  description: "hello",
+  lastUpdated: true,
   markdown: {
     math: true
   },
@@ -11,21 +13,53 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'About', link: 'about/me' },
+      // { text: 'Post', link: 'posts/1/content' },
+      // { text: 'Portfolio', link: 'about/me' }
+
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Post',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          {
+            text: 'Deep Learning',
+            items: [
+              { text: 'Can numerical differentiation replace backpropagation in a Multilayer Perceptron (MLP)?', link: 'posts/1/content' },
+            ]
+          }
         ]
+      },
+      {
+        text: 'Portfolio',
+        // items: [
+        //   { text: 'Markdown Examples', link: '/markdown-examples' },
+        //   { text: 'Runtime API Examples', link: '/api-examples' }
+        // ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/bom1215/' },
+      { icon: 'linkedin', link: 'https://www.linkedin.com/in/tbeom15/' }
+    ],
+    lastUpdated: {
+      text: 'Last Updated',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+    footer: {
+        message: 'Released under the MIT License.',
+        copyright: 'Copyright © 2025-Present Junbeom Lee'
+    },
+    search: {
+      provider: 'local'
+    },
+    editLink: {
+      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path'
+    }
   }
 })
--- docs/index.md
+++ docs/index.md
@@ -3,23 +3,32 @@
 layout: home
 
 hero:
-  name: "Junbeom blog"
-  text: "blog"
-  tagline: My great project tagline
+  name: "Junbeom Lee"
+  # text: "blog"
+  tagline: Software Engineer
+  image:
+    src: /profile.jpg
+    alt: VitePress
+    style: "border-radius: 50%;"
+
   actions:
     - theme: brand
-      text: Markdown Examples
+      text: Blog posts
       link: /markdown-examples
     - theme: alt
       text: API Examples
       link: /api-examples
 
-features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+# features:
+#   - icon: 🛠️
+#     title: Simple and minimal, always
+#     details: Lorem ipsum...
+#   - title: Feature A
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature B
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature C
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
+
 
